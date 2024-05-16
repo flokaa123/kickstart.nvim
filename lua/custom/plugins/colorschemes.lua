a010833bc33d6e061b070b9c5e75c6dc944bf689
Lackermeier, Florian <Florian.Lackermeier@vector.com>
2024-05-16 14:34:39 +0200

    More stuff

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -1,4 +1,6 @@
 return{
+  {
+    -- Gruvbox colorscheme
     'sainnhe/gruvbox-material',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
@@ -10,4 +12,20 @@ return{
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
     end,
+  },
+  {
+    -- catpuccin colorscheme
+    "catppuccin/nvim",
+    priority = 1000,
+  },
+  {
+    -- kanagawa.nvim colorscheme
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
+  },
+  {
+    -- onedark colorscheme
+    'navarasu/onedark.nvim',
+    priority = 1000,
+  },
 }
--- lua/custom/plugins/vim_fugitive.lua
+++ lua/custom/plugins/vim_fugitive.lua
@@ -1,6 +1,6 @@
 return {
   'tpope/vim-fugitive',
   config = function()
-
+    vim.keymap.set('n', '<leader>G', vim.cmd.G, { desc = 'Open [G]it Window' })
   end,
 }
